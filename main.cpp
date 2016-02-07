cc9f9f2216d021aef2be90bd278250775247c2f5
Aleksandar Minja <only1sale@gmail.com>
2016-02-07 12:12:54 +0100

    Finished Solution

--- main.cpp
+++ main.cpp
@@ -1,9 +1,18 @@
-#include <iostream>
-
-using namespace std;
+#include <iostream> //cout and endl
+#include <iomanip> //setw() and right-alignment
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    for(int i = 1; i <= 12; ++i)
+    {
+        for(int j = 1; j <= 12; ++j)
+        {
+            int k = (j==1) ? 2 : 4; //strip out leading spaces
+            //setw(k) - set width of the next output to k places
+            //right - make the output text right-aligned
+            std::cout << std::setw(k) << std::right << i*j;
+        }
+        //print in matrix form
+        std::cout << std::endl;
+    }
 }
